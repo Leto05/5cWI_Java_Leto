cb5bac1fe197763c2fefae174b859d8f89518f62
Leto05 <leto.harm@student.htldornbirn.at>
2024-12-11 12:14:53 +0100

    Exceptions

---
+++ projects/Sort/DataGenerator.java
@@ -0,0 +1,16 @@
+package projects.Sort;
+
+import java.util.Random;
+
+public class DataGenerator {
+
+    public static int[] GenerateNumbers(int size) {
+        int[] UnsortedNumbers = {};
+        Random r = new Random();
+        for (int i = 0; i < size; i++) {
+
+        }
+
+        return UnsortedNumbers;
+    }
+}
