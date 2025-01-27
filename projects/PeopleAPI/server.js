7ccd4b7e82e0347754abcf9fed3be84a559d9fcc
Leto05 <leto.harm@student.htldornbirn.at>
2025-01-27 12:17:20 +0100

    REST_People

--- projects/PeopleAPI/server.js
+++ projects/PeopleAPI/server.js
@@ -1,12 +1,23 @@
 const express = require("express")
+const bodyParser = require('body-parser');
 
 
 const app = express();
 const port = 3010;
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
+const data =
+    [
+        { name: "Hans", age: 18 },
+        { name: "William", age: 20 },
+        { name: "Flo", age: 19 },
+        { name: "Danielus", age: 18 },
+        { name: "Leto", age: 19 },
+    ]
 
-app.get("/", (req, res) => {
-    res.send("I am here");
+app.get("/peeople", (req, res) => {
+    res.send(data);
 })
 
 app.listen(port, () => console.log(`Hello world app listening on port ${port}!`))
